4872cfe0ba0688c5813c9951f95c9e0fa7a755bd
Arthur Ramos <arthursramoscontato@gmail.com>
2026-07-08 16:08:47 -0300

    Configurando a main

--- campo_minado/src/main.js
+++ campo_minado/src/main.js
@@ -1,7 +1,29 @@
-import './style.css'
+import './style.css';
+import './componentes/cabecalho.js';
+import './componentes/tabuleiro-jogo.js';
 
 document.querySelector('#app').innerHTML = `
+  <main>
+    <div id="container">
+      <cabecalho-jogo></cabecalho-jogo>
+      <tabuleiro-jogo></tabuleiro-jogo>
+    </div>
+  </main>
+`;
 
-`
+const cabecalho = document.querySelector('cabecalho-jogo');
+const tabuleiro = document.querySelector('tabuleiro-jogo');
 
+cabecalho.definirMinas(10);
 
+tabuleiro.addEventListener('jogo-iniciar', () => cabecalho.iniciarCronometro());
+tabuleiro.addEventListener('bandeiras-alteradas', (e) => cabecalho.atualizarBandeiras(e.detail.quantidadeBandeiradas));
+tabuleiro.addEventListener('jogo-encerrado', (e) => {
+  cabecalho.pararCronometro();
+  alert(e.detail.ganhou ? 'Você venceu!' : 'Você perdeu!');
+});
+
+cabecalho.addEventListener('reiniciar-jogo', () => {
+  cabecalho.reiniciar();
+  tabuleiro.reiniciar();
+});
